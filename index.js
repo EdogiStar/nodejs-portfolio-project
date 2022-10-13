77206845794ff364525bbcd1566608d35fba40ee
MuqsitX <muqsitxx@gmail.com>
2022-10-13 11:25:57 +0100

    Updated /public folder

--- index.js
+++ index.js
@@ -3,12 +3,40 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((request, response)=>{
+    // base URL
     if(request.url === '/') {
-        // let filepath = path.join(__dirname, 'public', 'index.html')
-        // fs.readFile(filepath, 'utf8', (err, data) =>{
+        let filepath = path.join(__dirname, 'public', 'home.html')
+        fs.readFile(filepath, 'utf8', (err, data) =>{
             response.writeHead(200, {'Content-Type' : 'text/html'})
-            response.end('<h1>Hello MuqsitX</h1>')
-        // })
+            response.end(data)
+        })
+    }
+
+    // /home URL
+    if(request.url === '/home') {
+        let filepath = path.join(__dirname, 'public', 'home.html')
+        fs.readFile(filepath, 'utf8', (err, data) =>{
+            response.writeHead(200, {'Content-Type' : 'text/html'})
+            response.end(data)
+        })
+    }
+
+    // /about URL
+    if(request.url === '/about') {
+        let filepath = path.join(__dirname, 'public', 'about.html')
+        fs.readFile(filepath, 'utf8', (err, data) =>{
+            response.writeHead(200, {'Content-Type' : 'text/html'})
+            response.end(data)
+        })
+    }
+
+    // /contact URL
+    if(request.url === '/contact') {
+        let filepath = path.join(__dirname, 'public', 'contact.html')
+        fs.readFile(filepath, 'utf8', (err, data) =>{
+            response.writeHead(200, {'Content-Type' : 'text/html'})
+            response.end(data)
+        })
     }
 });
 
